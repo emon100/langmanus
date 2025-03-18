42e83ed084c502127818f411ebca79ec68979bad
hetao <hetao7@pku.edu.cn>
2025-03-18 21:12:10 +0800

    feat: support deepseek chat

--- src/graph/nodes.py
+++ src/graph/nodes.py
@@ -88,7 +88,7 @@ def supervisor_node(state: State) -> Command[Literal[*TEAM_MEMBERS, "__end__"]]:
     messages = apply_prompt_template("supervisor", state)
     response = (
         get_llm_by_type(AGENT_LLM_MAP["supervisor"])
-        .with_structured_output(Router)
+        .with_structured_output(schema=Router, method="json_mode")
         .invoke(messages)
     )
     goto = response["next"]
